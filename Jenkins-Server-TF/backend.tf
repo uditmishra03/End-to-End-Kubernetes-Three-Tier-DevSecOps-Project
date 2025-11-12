a02a168b287846048bd48c7905346f4ce3926faa
Mishra, Udit <udit.mishra2@wipro.com>
2025-11-12 23:33:32 +0530

    Updating backend.tf for deprecated warning of dyanamodb param

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "eks-devsecops-bucket "
+    bucket         = "eks-devsecops-bucket"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "eks-devsecops-lock-files"
+    use_lockfile   = true
     encrypt        = true
   }
   required_version = ">=0.13.0"
