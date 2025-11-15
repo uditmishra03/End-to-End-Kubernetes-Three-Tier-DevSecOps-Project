04aaa62760dbc598866967125850ac1ebe7e276e
Mishra, Udit <udit.mishra2@wipro.com>
2025-11-16 01:19:57 +0530

    Testing CI/CD

--- Application-Code/backend/index.js
+++ Application-Code/backend/index.js
@@ -17,8 +17,8 @@ app.get('/healthz', (req, res) => {
     res.status(200).send('Healthy');
 });
 
-let lastReadyState = null;  
-// Readiness check to see if the server is ready to serve requests
+let lastReadyState = null;
+// Readiness check to see if the server is ready to serve requests     
 app.get('/ready', (req, res) => {
     // Here you can add logic to check database connection or other dependencies
     const isDbConnected = mongoose.connection.readyState === 1;
@@ -26,7 +26,7 @@ app.get('/ready', (req, res) => {
         console.log(`Database readyState: ${mongoose.connection.readyState}`);
         lastReadyState = isDbConnected;
     }
-    
+
     if (isDbConnected) {
         res.status(200).send('Ready');
     } else {
